15e4c743d5644c1efb7683c77be9677cd04e8950
Aniello-Bacarella <aniello.bacarella@gmail.com>
2025-11-30 07:39:42 -0500

    api.jsx updates

--- src/api.jsx
+++ src/api.jsx
@@ -1,3 +1,14 @@
 const BASE_URL = "https://fsa-crud-2aa9294fe819.herokuapp.com/api";
 const COHORT = "/2508-FTB-CT-WEB-PT";
 const API = BASE + COHORT;
+
+export async function fetchGuests() {
+  const res = await fetch(`${API}/guests`);
+  const data = await res.json();
+  return data;
+}
+
+export async function fetchGuestDetails(id) {
+  const data = await fetchGuestDetails(guests.id);
+  setSelectedGuest(data.guest);
+}

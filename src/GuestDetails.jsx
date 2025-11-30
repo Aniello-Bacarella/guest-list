72f9be49bc5a4c4ad4505571fcfa6e9f347ef920
Aniello-Bacarella <aniello.bacarella@gmail.com>
2025-11-30 07:23:02 -0500

    started guest details

---
+++ src/GuestDetails.jsx
@@ -0,0 +1,12 @@
+export default function GuestDetails({ guest, onBack}) {
+    const details = {
+        ...guest,
+        phone: "444-1243",
+        bio: "they like pizza",
+        job: "Delivery Driver",
+    };
+    
+    return (
+        
+    )
+}

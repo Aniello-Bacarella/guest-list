05200cbdbe2095fcf540c4f82762380a29789bd8
Aniello-Bacarella <aniello.bacarella@gmail.com>
2025-11-30 07:55:34 -0500

    fixed BASE_URL name in api.jsx

--- src/api.jsx
+++ src/api.jsx
@@ -1,6 +1,6 @@
 const BASE_URL = "https://fsa-crud-2aa9294fe819.herokuapp.com/api";
 const COHORT = "/2508-FTB-CT-WEB-PT";
-const API = BASE + COHORT;
+const API = BASE_URL + COHORT;
 
 export async function fetchGuests() {
   const res = await fetch(`${API}/guests`);

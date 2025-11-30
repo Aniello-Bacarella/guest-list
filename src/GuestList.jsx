4c8c6b7a2ef95da155bf1fcfd456e513433ab3dd
Aniello-Bacarella <aniello.bacarella@gmail.com>
2025-11-30 06:55:48 -0500

    guestlist.jsx updated

--- src/GuestList.jsx
+++ src/GuestList.jsx
@@ -1,4 +1,13 @@
-export default function GuestList({ guests, onSelect}) {
-    return (
-    )
+export default function GuestList({ guests, onSelect }) {
+  return (
+    <ul>
+      {guests.map((guest) => (
+        <li key={guest.id} onClick={() => onSelect(guest)}>
+          <p>
+            {guest.name} - {guest.email}
+          </p>
+        </li>
+      ))}
+    </ul>
+  );
 }

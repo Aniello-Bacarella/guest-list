1511b85c593ad5559e124cd3a6e4cd024199a9c7
Aniello-Bacarella <aniello.bacarella@gmail.com>
2025-11-30 06:45:02 -0500

    placeholder guests created

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,25 @@
+import GuestList from "./GuestList.jsx";
+import GuestDetails from "./GuestDetails.jsx";
+
+const placeholderGuests = [
+  { id: 1, name: "Anthony Bacarella", email: "anthony@masonschools.com" },
+  { id: 2, name: "Joe Bacarella", email: "joeb2001@yahoo.com" },
+  { id: 3, name: "Priya Rengaraj", emial: "priya123@gmail.com" },
+];
+
 export default function App() {
-  return <></>;
+  const [selectedGuest, setSelectedGuest] = useState(null);
+  return (
+    <div>
+      <h1>Convention Guests</h1>
+      {selectedGuest ? (
+        <GuestDetails
+          guest={selectedGuest}
+          onBack={() => setSelectedGuest(null)}
+        />
+      ) : (
+        <GuestList guests={placeholderGuests} onSelect={setSelectedGuest} />
+      )}
+    </div>
+  );
 }

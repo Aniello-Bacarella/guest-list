fc6c0690b5f1a8afad00c921cd4bf670933ba8db
Aniello-Bacarella <aniello.bacarella@gmail.com>
2025-11-30 07:35:44 -0500

    added api.jsx and base + cohort

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
+import { useState } from "react";
 import GuestList from "./GuestList.jsx";
 import GuestDetails from "./GuestDetails.jsx";
 
 const placeholderGuests = [
   { id: 1, name: "Anthony Bacarella", email: "anthony@masonschools.com" },
   { id: 2, name: "Joe Bacarella", email: "joeb2001@yahoo.com" },
-  { id: 3, name: "Priya Rengaraj", emial: "priya123@gmail.com" },
+  { id: 3, name: "Priya Rengaraj", email: "priya123@gmail.com" },
 ];
 
 export default function App() {

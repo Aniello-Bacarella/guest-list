f8f02772078fa4b18f62750f36626d5cbb0231a4
Aniello-Bacarella <aniello.bacarella@gmail.com>
2025-11-30 07:27:02 -0500

    placeholder data setup done

--- src/GuestDetails.jsx
+++ src/GuestDetails.jsx
@@ -1,12 +1,19 @@
-export default function GuestDetails({ guest, onBack}) {
-    const details = {
-        ...guest,
-        phone: "444-1243",
-        bio: "they like pizza",
-        job: "Delivery Driver",
-    };
-    
-    return (
-        
-    )
+export default function GuestDetails({ guest, onBack }) {
+  const details = {
+    ...guest,
+    phone: "444-1243",
+    bio: "they like pizza",
+    job: "Delivery Driver",
+  };
+
+  return (
+    <div>
+      <h2>{details.name}</h2>
+      <p>Email: {details.email}</p>
+      <p>Phone: {details.phone}</p>
+      <p>Job: {details.job}</p>
+      <p>Bio: {details.bio}</p>
+      <button onClick={onBack}>Back</button>
+    </div>
+  );
 }
